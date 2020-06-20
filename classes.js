c87fa5bf16cd8dfe9a2b4bf2c9c0c58f6131ba63
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-06-19 19:10:22 -0700

    add a classes.js file for when a class may be needed and set up first database in MySQL

---
+++ classes.js
@@ -0,0 +1,1 @@
+// You may wish to have a separate file containing functions for performing specific SQL queries you'll need to use. Could a constructor function or a class be helpful for organizing these?
