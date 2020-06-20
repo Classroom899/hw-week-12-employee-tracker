37884cfa13d2dcccad4479bb729ea43439419ca5
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-06-19 19:31:39 -0700

    add var connection = mysql.createConnection and add the port, username, and password along with database that is being used from MySQL

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,1 +1,15 @@
 var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "access",
+
+  // Your password
+  password: "",
+  database: "company_db",
+});
