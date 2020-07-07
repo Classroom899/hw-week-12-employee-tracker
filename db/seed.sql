b8daa5fcffd6f43e95eeef59c9079c1ac00f45de
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-07-06 22:09:02 -0700

    Add LEFT JOIN employee AS manager

--- db/seed.sql
+++ db/seed.sql
@@ -13,3 +13,17 @@ VALUES ("FCA Engineer Intern", 1000.00, 1);
 INSERT INTO employee (firstName, lastName, role_id, manager_id)
 VALUES ("Joe", "Smith", 2, 4)
 
+SELECT employee.id, employee.firstName, employee.lastName, role.title, role.salary, department.name, manager.firstName
+FROM employee
+LEFT JOIN role
+ON
+employee.role_id=role.id -- order matters when doing left joins
+
+LEFT JOIN department 
+ON
+role.department_id=department.id
+
+LEFT JOIN employee AS manager -- alias used here
+ON
+employee.id=manager.id
+
